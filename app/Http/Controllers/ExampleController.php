5bf65e44748a10e656d9de97110cfc689613604b
basmalaosamaa <basmalaosama245@gmail.com>
2024-07-17 17:09:13 +0300

    Update ExampleController.php

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -10,7 +10,7 @@ class ExampleController extends Controller
     //     return view('login');
     // }
 
-    function Contact () {
+    function contact () {
         return view('Contact');
     }
     function data () {
